727c75c954266e3b4f8955e0beafa3468d563589
masudrana08 <rana028511@gmail.com>
2020-07-22 17:28:54 +0600

    mBank finally done

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ var loginArea = document.getElementById("login-area");
 var submit = document.getElementById("enter");
 var transactionArea = document.getElementById("transaction-area");
 
-// submit.addEventListener("click",function(){
-//     loginArea.style.display = "none";
-//     transactionArea.style.display = "block";
-// })
-
 submit.addEventListener("click",function(){
-    transactionArea.style.display="block";
-    document.body.replaceChild(transactionArea,loginArea);
+    loginArea.style.display = "none";
+    transactionArea.style.display = "block";
 })
+
+// submit.addEventListener("click",function(){
+//     transactionArea.style.display="block";
+//     document.body.replaceChild(transactionArea,loginArea);
+// })
 
 
 // calculation area for deposite and withdraw
@@ -26,25 +26,54 @@ var intWithdrawalAmount = parseInt(withdrawalAmount.innerHTML);
 var totalAmount=document.getElementById("total-amount")
 var intTotalAmount = parseInt(totalAmount.innerHTML)
 
-
-depositeAmountCalc.addEventListener("keypress",function(event){
-    if(event.keyCode==13){
+// function depositeFunc(event){
+//     if(event.keyCode==13){
         
-        intDepositedAmount+=parseInt(event.target.value);
-        depositedAmount.innerHTML=intDepositedAmount;
-        intTotalAmount+=parseInt(event.target.value);
-        totalAmount.innerHTML=intTotalAmount;
-        intWithdrawalAmount+=parseInt(event.target.value);
-        withdrawalAmount.innerHTML=intWithdrawalAmount;
-    }
-})
+//         intDepositedAmount+=parseInt(event.target.value);
+//         depositedAmount.innerHTML=intDepositedAmount;
+//         intTotalAmount+=parseInt(event.target.value);
+//         totalAmount.innerHTML=intTotalAmount;
+//         intWithdrawalAmount+=parseInt(event.target.value);
+//         withdrawalAmount.innerHTML=intWithdrawalAmount;
+//         depositeAmountCalc.value = ""
+//     }
+// }
+// depositeAmountCalc.addEventListener("keypress",depositeFunc)
 
-withdrawAmountCalc.addEventListener("keypress",function(event){
-    if(event.keyCode==13){
-        intWithdrawalAmount -= parseInt(event.target.value);
-        withdrawalAmount.innerHTML=intWithdrawalAmount;
+// function withdrawFunc(event){
+//     if(event.keyCode==13){
+//         intWithdrawalAmount -= parseInt(event.target.value);
+//         withdrawalAmount.innerHTML=intWithdrawalAmount;
 
-        intTotalAmount -=parseInt(event.target.value);
-        totalAmount.innerHTML = intTotalAmount;
-    }
+//         intTotalAmount -=parseInt(event.target.value);
+//         totalAmount.innerHTML = intTotalAmount;
+//         withdrawAmountCalc.value = "";
+//     }
+// }
+
+// withdrawAmountCalc.addEventListener("keypress",withdrawFunc)
+
+
+var addAmount = document.getElementById("add-amount");
+var releaseAmount = document.getElementById("release-amount");
+
+addAmount.addEventListener("click",function(event){
+    var intDepAmount = parseInt(depositedAmount.innerText)
+    intDepAmount+=parseInt(depositeAmountCalc.value);
+    depositedAmount.innerText = intDepAmount;
+   
+    
+    intTotalAmount += parseInt(depositeAmountCalc.value);
+    totalAmount.innerText=intTotalAmount;
+    depositeAmountCalc.value = "";
+    })
+
+releaseAmount.addEventListener("click",function(event){
+    intWithdrawalAmount+=parseInt(withdrawAmountCalc.value);
+    withdrawalAmount.innerText=intWithdrawalAmount;
+    intTotalAmount -= parseInt(withdrawAmountCalc.value);
+    totalAmount.innerText=intTotalAmount;
+
+    withdrawAmountCalc.value="";
 })
+  
