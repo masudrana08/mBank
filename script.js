dd521180624875b2794f2bbcd7d572343a57cd37
masudrana08 <rana028511@gmail.com>
2020-07-22 08:51:40 +0600

    mBank design and logic process complete

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-let ul=document.getElementById("language");
+var loginArea = document.getElementById("login-area");
+var submit = document.getElementById("enter");
+var transactionArea = document.getElementById("transaction-area");
 
-for (var i=0; i<10; i++){
-    var serial = "serial"+i;
+// submit.addEventListener("click",function(){
+//     loginArea.style.display = "none";
+//     transactionArea.style.display = "block";
+// })
 
-    console.log(serial);
-    serial =ul.cloneNode(true);
-    document.body.appendChild(serial);
-}
+submit.addEventListener("click",function(){
+    transactionArea.style.display="block";
+    document.body.replaceChild(transactionArea,loginArea);
+})
+
+
+// calculation area for deposite and withdraw
+var depositeAmountCalc = document.getElementById("deposite-amount-calc");
+var withdrawAmountCalc = document.getElementById("withdraw-amount-calc");
+
+var depositedAmount = document.getElementById("deposited-amount");
+var intDepositedAmount =parseInt(depositedAmount.innerHTML);
+
+var withdrawalAmount = document.getElementById("withdrawal-amount");
+var intWithdrawalAmount = parseInt(withdrawalAmount.innerHTML);
+
+var totalAmount=document.getElementById("total-amount")
+var intTotalAmount = parseInt(totalAmount.innerHTML)
+
+
+depositeAmountCalc.addEventListener("keypress",function(event){
+    if(event.keyCode==13){
+        
+        intDepositedAmount+=parseInt(event.target.value);
+        depositedAmount.innerHTML=intDepositedAmount;
+        intTotalAmount+=parseInt(event.target.value);
+        totalAmount.innerHTML=intTotalAmount;
+        intWithdrawalAmount+=parseInt(event.target.value);
+        withdrawalAmount.innerHTML=intWithdrawalAmount;
+    }
+})
+
+withdrawAmountCalc.addEventListener("keypress",function(event){
+    if(event.keyCode==13){
+        intWithdrawalAmount -= parseInt(event.target.value);
+        withdrawalAmount.innerHTML=intWithdrawalAmount;
+
+        intTotalAmount -=parseInt(event.target.value);
+        totalAmount.innerHTML = intTotalAmount;
+    }
+})
